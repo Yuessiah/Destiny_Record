1e421bfd69b4b480c078b8407edc70a35b5afbad
Yuessiah <Yuessiah@gmail.com>
2015-08-21 23:58:32 +0800

    El Psy Congroo

---
+++ TestOnlineJudge_19.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+const int maxn = 100000;
+int main()
+{
+	int t, stk[maxn], tra[maxn];
+	scanf("%d", &t);
+	while(t--) {
+		int n, top = -1; 
+		scanf("%d", &n);
+		for(int i = 0; i < n; i++) scanf("%d", &tra[i]);
+
+		int i = n-1;
+		while(n != 0)
+			if((top < 0 || stk[top] != n) && i >= 0) stk[++top] = tra[i--];
+			else { if(stk[top] == n) top--; n--; }
+
+		top != -1 ? printf("No") : printf("Yes");
+		putchar('\n');
+	}
+	return 0;
+}
