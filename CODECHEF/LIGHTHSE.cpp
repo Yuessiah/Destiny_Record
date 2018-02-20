5c53004d8b234a3ebea668331d6fe2f99fc3f732
Yuessiah <yuessiah@gmail.com>
2018-02-20 19:51:17 +0800

    (ゝ∀･)

--- CODECHEF/LIGHTHSE.cpp
+++ CODECHEF/LIGHTHSE.cpp
@@ -10,42 +10,38 @@ int main()
 	scanf("%d", &T);
 
 	while(T--) {
-		int lo_y =  INF, r_lo_x = -INF, l_lo_x = INF, r_lo_i, l_lo_i;
-		int hi_y = -INF, r_hi_x = -INF, l_hi_x = INF, r_hi_i, l_hi_i;
+		int max_x = -INF, max_y = -INF, min_x = INF, min_y = INF;
+		int max_i, min_i;
 		scanf("%d", &N);
 
 		for(int i = 1; i <= N; i++) {
 			scanf("%d%d", &x[i], &y[i]);
-			if(y[i] > hi_y) hi_y = y[i];
-			if(y[i] < lo_y) lo_y = y[i];
+			if(x[i] > max_x) max_x = x[i];
+			if(y[i] > max_y) max_y = y[i], max_i = i;
+			if(x[i] < min_x) min_x = x[i];
+			if(y[i] < min_y) min_y = y[i], min_i = i;
 		}
 
-		for(int i = 1; i <= N; i++)  {
-			if(y[i] == hi_y) {
-				if(x[i] > r_hi_x) r_hi_x = x[i], r_hi_i = i;
-				if(x[i] < l_hi_x) l_hi_x = x[i], l_hi_i = i;
+		int idx = 0;
+		char res[2];
+		for(int i = 1; i <= N && !idx; i++) {
+			if(x[i] == max_x) {
+				res[1] = 'W';
+				if(y[i] == max_y) res[0] = 'S', idx = i;
+				if(y[i] == min_y) res[0] = 'N', idx = i;
 			}
-			if(y[i] == lo_y) {
-				if(x[i] > r_lo_x) r_lo_x = x[i], r_lo_i = i;
-				if(x[i] < l_lo_x) l_lo_x = x[i], l_lo_i = i;
+			if(x[i] == min_x) {
+				res[1] = 'E';
+				if(y[i] == max_y) res[0] = 'S', idx = i;
+				if(y[i] == min_y) res[0] = 'N', idx = i;
 			}
 		}
 
-		bool r_hi_cnt = false, l_hi_cnt = false;
-		bool r_lo_cnt = false, l_lo_cnt = false;
-		for(int i = 1; i <= N; i++) {
-			if(x[i] > r_hi_x) r_hi_cnt = true;
-			if(x[i] < l_hi_x) l_hi_cnt = true;
-			if(x[i] > r_lo_x) r_lo_cnt = true;
-			if(x[i] < l_lo_x) l_lo_cnt = true;
+		if(idx) printf("1\n%d %c%c\n", idx, res[0], res[1]);
+		else {
+			if(x[max_i] > x[min_i]) printf("2\n%d SW\n%d NE\n", max_i, min_i);
+			else printf("2\n%d SE\n%d NW\n", max_i, min_i);
 		}
-
-		if     (!r_hi_cnt) printf("1\n%d SW\n", r_hi_i);
-		else if(!l_hi_cnt) printf("1\n%d SE\n", l_hi_i);
-		else if(!r_lo_cnt) printf("1\n%d NW\n", r_lo_i);
-		else if(!l_lo_cnt) printf("1\n%d NE\n", l_lo_i);
-		else if(r_hi_x > r_lo_x) printf("2\n%d SW\n%d NE\n", r_hi_i, r_lo_i);
-		else printf("2\n%d SE\n%d NW\n", r_hi_i, r_lo_i);
 	}
 
 	return 0;
