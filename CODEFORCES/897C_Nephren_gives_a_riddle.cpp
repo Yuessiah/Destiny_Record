96b4c142a4e2928f899557e3c8683858a175b6af
Yuessiah <yuessiah@gmail.com>
2018-09-07 21:23:11 +0800

    (ゝ∀･)

--- CODEFORCES/897C_Nephren_gives_a_riddle.cpp
+++ CODEFORCES/897C_Nephren_gives_a_riddle.cpp
@@ -1,51 +1,47 @@
 // This solution is to imitate dotorya's code.
-#include<iostream>
-#include<algorithm>
+#include<cstdio>
 #include<cstring>
-using namespace std;
+#include<algorithm>
+using std::min;
 
-const int maxn = 1e5 + 100;
-const long long INF = 0x3f3f3f3f3f3f3f3f;
+int const maxn = 1e5 + 100;
+long long const INF = 0x3f3f3f3f3f3f3f3f;
 
-const char f0[] = "What are you doing at the end of the world? Are you busy? Will you save us?";
-const char fi[] = "What are you doing while sending \"@\"? Are you busy? Will you send \"@\"?";
-const int f0_len = strlen(f0), fi_len = strlen(fi);
+char const f0[] = "What are you doing at the end of the world? Are you busy? Will you save us?";
+char const f[]  = "What are you doing while sending \"@\"? Are you busy? Will you send \"@\"?";
+int const f_len = strlen(f);
 
-long long f_l[maxn]; //length of f_i
+int q, N;
+long long K, f_l[maxn]; //length of f_i
 
 char dfs(int n, long long k) {
-	if(n == 0) return f0[k];
+  if(n == 0 && k < f_l[0]) return f0[k];
 
-	for(int c = 0; c < fi_len; c++) {
-		if(fi[c] == '@') {
-			if(k < f_l[n-1]) return dfs(n-1, k);
-			k -= f_l[n-1];
-		} else {
-			if(k == 0) return fi[c];
-			k--;
-		}
-	}
+  for(int c = 0; c < f_len; c++) {
+    if(f[c] == '@') {
+      if(k < f_l[n-1]) return dfs(n-1, k);
+      k -= f_l[n-1];
+    } else {
+      if(k == 0) return f[c];
+      k--;
+    }
+  }
 
-	return '.';
+  return '.';
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
+  //init
+  f_l[0] = strlen(f0);
+  for(int i = 1; i < maxn; i++) f_l[i] = min(INF, 2*f_l[i-1] + f_len - 2);
 
-	//init
-	f_l[0] = f0_len;
-	for(int i = 1; i < maxn; i++) f_l[i] = min(INF, 2*f_l[i-1] + fi_len - 2);
+  scanf("%d", &q);
 
-	long long q, n, k;
-	cin >> q;
+  while(q--) {
+    scanf("%d%lld", &N, &K);
+    putchar(dfs(N, K-1));
+  }
 
-	while(q--) {
-		cin >> n >> k;
-		if(k > f_l[n]) cout << '.';
-		else cout << dfs(n, k-1);
-	}
-
-	return 0;
+  return 0;
 }
