716896bf5300a74a756cafb864f09585659151ae
yuessiah <yuessiah@gmail.com>
2020-03-09 22:16:39 +0800

    (ゝ∀･)

--- CODEFORCES/1307B_Cow_and_Friend.cpp
+++ CODEFORCES/1307B_Cow_and_Friend.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int const maxn = 1e5 + 10;
 
-int t, n, x, a[maxn];
+int t, n, x, a;
 
 int main()
 {
@@ -15,9 +15,9 @@ int main()
     int mx = 0;
     bool eq = false;
     for(int i = 0; i < n; i++) {
-      scanf("%d", &a[i]);
-      if(a[i] == x) eq = true;
-      mx = max(mx, a[i]);
+      scanf("%d", &a);
+      if(a == x) eq = true;
+      mx = max(mx, a);
     }
 
     if(eq) puts("1");
--- CODEFORCES/1307D_Cow_and_Fields.cpp
+++ CODEFORCES/1307D_Cow_and_Fields.cpp
@@ -3,20 +3,22 @@ using namespace std;
 
 int const maxn = 2e5 + 10;
 
-int n, m, k, x, y;
-vector<int> E[maxn], a;
+int n, m, k, a, x, y;
+vector<int> E[maxn], ord;
+vector<bool> sp;
 
 void bfs(int s, vector<int> &d) {
   queue<int> q;
-  set<int> vis;
   q.push(s);
-  vis.insert(s);
+  d.assign(n+1, -1);
   d[s] = 0;
+
   while(q.size()) {
     int u = q.front(); q.pop();
+    if(s == 1 && sp[u]) ord.push_back(u);
+
     for(int v: E[u]) {
-      if(vis.count(v)) continue;
-      vis.insert(v);
+      if(d[v] != -1) continue;
       d[v] = d[u] + 1;
       q.push(v);
     }
@@ -27,22 +29,23 @@ int main()
 {
   scanf("%d%d%d", &n, &m, &k);
 
-  a.resize(k);
-  for(int i = 0; i < k; i++) scanf("%d", &a[i]);
+  sp.assign(n+1, false);
+  for(int i = 0; i < k; i++) scanf("%d", &a), sp[a] = true;
   for(int i = 0; i < m; i++) {
     scanf("%d%d", &x, &y);
     E[x].push_back(y);
     E[y].push_back(x);
   }
 
-  vector<int> d1(n+1), dn(n+1);
+  vector<int> d1, dn;
   bfs(1, d1);
   bfs(n, dn);
 
-  sort(a.begin(), a.end(), [&](int x, int y) { return d1[x] < d1[y]; });
-
-  int ans = 0;
-  for(int i = 0; i+1 < k; i++) ans = max(ans, d1[a[i]] + 1 + dn[a[i+1]]);
+  int ans = 0, d1_mx = 0;
+  for(int i = 0; i+1 < ord.size(); i++) {
+    d1_mx = max(d1_mx, d1[ord[i]]);
+    ans = max(ans, d1_mx + 1 + dn[ord[i+1]]);
+  }
 
   printf("%d\n", min(ans, d1[n]));
 
