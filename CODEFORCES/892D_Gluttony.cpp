42d6d2036f7bae056c3934e625215d3e4d69ea6d
yuessiah <yuessiah@gmail.com>
2021-01-21 23:43:25 +0800

    (ゝ∀･)

---
+++ CODEFORCES/892D_Gluttony.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int constexpr maxn = 25;
+
+int n, a[maxn], c[maxn];
+
+int main()
+{
+  cin >> n;
+  for(int i = 0; i < n; i++) cin >> a[i], c[i] = a[i];
+  sort(c, c+n);
+
+  for(int i = 0; i < n; i++) {
+    int p = lower_bound(c, c+n, a[i]) - c;
+    cout << c[(p-1+n)%n] << ' ';
+  }
+  cout << endl;
+
+  return 0;
+}
